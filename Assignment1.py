2d14fe0272fb9449b77bfbe4825c07af05687600
g1jung <56431900+g1jung@users.noreply.github.com>
2025-10-27 20:21:32 -0700

    Final code

--- Assignment1.py
+++ Assignment1.py
@@ -1,9 +1,20 @@
 def insertionSort(arr):
-    length = len(arr);
+    length = len(arr)
 
     if length < 2:
         return  #no need to sort less than 2
     
     for i in range(1, length):
         currNum = arr[i]
+
+        loopNum = i-1
+        while loopNum >= 0 and currNum < arr[loopNum]:
+            arr[loopNum+1] = arr[loopNum]
+            loopNum -= 1
         
+        arr[loopNum+1] = currNum
+
+
+arr = [12, 39, 22, 67, 2, 15]
+insertionSort(arr)
+print(arr)
